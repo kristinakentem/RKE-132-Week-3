156c7f8c1c971a7e143baa631d5dc9af06cf3c98
Kristina Kentem. <kristina.kentem@tktk.ee>
2024-06-01 12:16:57 +0300

    add week3 code

---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,30 @@
+﻿int weekDay = 10;  //Convert.ToInt32(weekDay); //0-6
+
+switch(weekDay)// väärtus, mida kontrollitakse
+{
+    case 0: //case 0:== if (weekDay ==0)
+        Console.WriteLine("Its Sunday");
+        break;
+    case 1:
+        Console.WriteLine("Its Monday!");
+        break;
+    case 2:
+        Console.WriteLine("Its Tuesday!");
+        break;
+    case 3:
+        Console.WriteLine("Its Wednesday!");
+        break;
+    case 4:
+        Console.WriteLine("Its Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Its Friday");
+        break;
+    case 6:
+        Console.WriteLine("Its Saturday");
+        break;
+    default:
+        Console.WriteLine("Oops. Your calender must be broken.");
+        break;
+}
+Console.WriteLine("Have a nice day!");
---
+++ Week3ifAndSwitch/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//moodulus-%
+
+using System.ComponentModel;
+
+int a = 25;
+int b = 8;
+
+int c = a % b; //+/-/*-/
+Console.WriteLine(c);
+
